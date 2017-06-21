ec776546df893acb6587e5786b52a6397b1f1fcd
syed saifi <syed.saifi@oracle.com>
2017-06-22 00:56:03 +0530

    first commit

---
+++ src/main/java/com/example/app/dao/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package com.example.app.dao;
+
+import java.util.List;
+
+import com.example.app.entity.EmployeeEntity;
+ 
+public interface EmployeeDAO
+{
+    public void addEmployee(EmployeeEntity employee);
+    public List<EmployeeEntity> getAllEmployees();
+    public void deleteEmployee(Integer employeeId);
+}
